d5469b5c32f9d1c1c4c7d9a83f4aea7b538285ce
STAR COMPUTER <digitalboyofficial123@gmail.com>
2025-06-19 05:18:39 -0700

    file is new updated

--- Chapter 6 Problems/Problem(1).py
+++ Chapter 6 Problems/Problem(1).py
@@ -7,4 +7,7 @@ elif num==0:
     print("num is zero")
 else:
     print("num not found")
+    # This will check that num is not negative, positive, or zero.
+# The last else statement is redundant and will never be executed until the above conditions are met.
+# The code will always print "num is positive" since num is 89.
     
