3ec1b198bac64c2f1ff1306931e3fc06db1c9c70
STAR COMPUTER <digitalboyofficial123@gmail.com>
2025-06-30 08:34:08 -0700

    new repo

--- Chapter 7 Problems/problem (15).py
+++ Chapter 7 Problems/problem (15).py
@@ -1,5 +1,10 @@
-Str = "Hey! My name is Sadeeq and I wanna master Python and I am trying my best!"
-Vowel_list = "AEIOUaeiou"
-for char in Str:
-    if char in Vowel_list:
-        print(char,end=" ")
+# Str = "Hey! My name is Sadeeq and I wanna master Python and I am trying my best!"
+# Vowel_list = "AEIOUaeiou"
+# for char in Str:
+#     if char in Vowel_list:
+#         print(char,end=" ")
+
+for i in range(1,16):
+    y = f"Chapter 8 Problems/Problem ({i}).py"
+    with open(y, 'w') as f:
+        print(y)
--- Chapter 7 Problems/problem (4).py
+++ Chapter 7 Problems/problem (4).py
@@ -1,5 +1,11 @@
-num = int(input("Enter your number of terms: "))
-a,b = 0,1
-for i in range(num):
-    print(a,end="")
-    a,b = b,a+b
+# num = int(input("Enter your number of terms: "))
+# a,b = 0,1
+# for i in range(num):
+#     print(a,end="")
+#     a,b = b,a+b
+ 
+
+for i in range(1,16):
+    y = f"Chapter 8 Problems/Problem ({i}).py"
+    with open(y, 'w') as f:
+        print(y)
